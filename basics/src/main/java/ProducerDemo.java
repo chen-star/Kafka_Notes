9c0f6c3758e0d2002ef4eb9531352d07cc97899a
Alex Chen <jiaxinc31@gmail.com>
2022-07-27 10:08:54 -0700

    init

---
+++ basics/src/main/java/ProducerDemo.java
@@ -0,0 +1,35 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_topic_0", "hello world");
+
+        // send data - async
+        producer.send(producerRecord);
+
+        // flush and close the producer - sync
+        producer.flush();
+        producer.close();
+    }
+}
---
+++ basics/src/main/java/ProducerDemo_Callback.java
@@ -0,0 +1,51 @@
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo_Callback {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo_Callback.class.getSimpleName());
+
+    public static void main(String[] args) {
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            // create a producer record
+            ProducerRecord<String, String> producerRecord =
+                    new ProducerRecord<>("demo_topic_0", "hello world");
+
+            // send data - async
+            // Callback changes
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    // executes everytime a record is successfully sent or an exception is thrown
+                    if (exception == null) {
+                        log.info("Received new metadata \n" +
+                                "Topic: " + metadata.topic() + "\n" +
+                                "Partition: " + metadata.partition() + "\n" +
+                                "Offset: " + metadata.offset() + "\n" +
+                                "Timestamp: " + metadata.timestamp());
+                    } else {
+                        log.error("Error while producing", exception);
+                    }
+                }
+            });
+
+        }
+
+        // flush and close the producer - sync
+        producer.flush();
+        producer.close();
+    }
+}
